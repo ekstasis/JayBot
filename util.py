89a934c11fb6200448bffa228b0e67f82861b6c9
James Baxter <8950586+ekstasis@users.noreply.github.com>
2021-06-13 14:13:23 -0400

    Remove Mac-Debian host replacement

--- util.py
+++ util.py
@@ -3,6 +3,6 @@ import socket
 
 def get_host() -> str:
     host = socket.gethostname()
-    if host == 'JB-MBP-15':
-        host = 'debian_from_mac'
+    # if host == 'JB-MBP-15':
+    #     host = 'debian_from_mac'
     return host
