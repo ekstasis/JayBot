77d9e49b49763de0ed2e4e4b66337494e26ee052
James Baxter <8950586+ekstasis@users.noreply.github.com>
2021-06-13 14:16:35 -0400

    Set up ETH alerts, etc.
    Also:
    - Test mode
    - New messenger ID system
    - Set alert formatting to handle small and big numbers

--- alert_runner.py
+++ alert_runner.py
@@ -6,7 +6,7 @@ import datetime as dt
 import alert.alerts as alerts
 from alert.analyzers import PeriodAnalyzer
 import util
-import alert.messenger as chat
+from alert.messenger import TelegramChat, XLM_CHAT_IDS, OTHER_CHAT_IDS, ETH_CHAT_IDS
 
 
 def init_logging(level):
@@ -16,6 +16,8 @@ def init_logging(level):
 
 
 def heartbeat():
+    """ This prints '.' every minute to indicate the alerts are running
+    """
     now = dt.datetime.utcnow()
     msg = f'\n{now.hour}:00' if now.minute == 0 else '.'
     print(msg, end='', flush=True)
@@ -26,37 +28,61 @@ if __name__ == '__main__':
     debug = False
     if len(args) >= 2:
         debug = args[1] == 'debug'
+    should_test = len(args) == 3 and args[2] == 'test'
     if debug:
         init_logging(logging.DEBUG)
     else:
         init_logging(logging.INFO)
-    logging.info('Starting Alerts ...')
-
-    # test_chat = chat.TelegramChat.create_bot('Test')
 
     analyzer = PeriodAnalyzer()
-    table = 'matches_xlm'
 
-    min_diff_chat = chat.TelegramChat.create_bot('1_min_diff')
-    diff_1m = alerts.MinuteDiffAlert(name='1m diff', table=table, messenger=min_diff_chat, analyzer=analyzer)
-    diff_1m.set_threshold(300000)
+    xlm_table = 'matches_xlm'
+    eth_table = 'matches_eth'
 
-    hour_diff_chat = chat.TelegramChat.create_bot('1_hr_diff')
-    diff_1h = alerts.HourAlert(name='1h_diff', table=table, messenger=hour_diff_chat, analyzer=analyzer)
-    diff_1h.set_threshold(0)
+    """ XLM Alerts """
+    xlm_min_diff_chat = TelegramChat(chat_id=XLM_CHAT_IDS['1_min_diff'])
+    xlm_diff_1m = alerts.MinuteDiffAlert(name='xlm 1m diff', table=xlm_table,
+                                         messenger=xlm_min_diff_chat, analyzer=analyzer, test=should_test)
+    xlm_diff_1m.set_threshold(300_000)
 
-    four_hour_chat = chat.TelegramChat.create_bot('FOUR_HOUR')
-    diff_4h = alerts.FourHourAlert(name='4h_diff', table=table, messenger=four_hour_chat, analyzer=analyzer)
-    diff_1h.set_threshold(0)
+    xlm_hour_diff_chat = TelegramChat(chat_id=XLM_CHAT_IDS['1_hr_diff'])
+    xlm_diff_1h = alerts.HourAlert(name='xlm 1h_diff', table=xlm_table, messenger=xlm_hour_diff_chat,
+                                   analyzer=analyzer, test=should_test)
+    xlm_diff_1h.set_threshold(0)
 
-    alerts = [diff_1m, diff_1h, diff_4h]
+    xlm_four_hour_chat = TelegramChat(chat_id=XLM_CHAT_IDS['FOUR_HOUR'])
+    xlm_diff_4h = alerts.FourHourAlert(name='xlm 4h_diff', table=xlm_table, messenger=xlm_four_hour_chat,
+                                       analyzer=analyzer, test=should_test)
+    xlm_diff_1h.set_threshold(0)
 
-    error_chat = chat.TelegramChat.create_bot('JayBotErrors')
+    """ ETH Alerts """
+    eth_min_diff_chat = TelegramChat(chat_id=ETH_CHAT_IDS['ETH 1-Minute'])
+    eth_diff_1m = alerts.MinuteDiffAlert(name='eth 1m diff', table=eth_table, messenger=eth_min_diff_chat,
+                                         analyzer=analyzer, test=should_test)
+    eth_diff_1m.set_threshold(200)
+
+    eth_hour_diff_chat = TelegramChat(chat_id=ETH_CHAT_IDS['ETH 1-Hour'])
+    eth_diff_1h = alerts.HourAlert(name='eth_1h_diff', table=eth_table, messenger=eth_hour_diff_chat,
+                                   analyzer=analyzer, test=should_test)
+    eth_diff_1h.set_threshold(0)
+
+    eth_4hour_diff_chat = TelegramChat(chat_id=ETH_CHAT_IDS['ETH 4-Hour'])
+    eth_diff_4h = alerts.FourHourAlert(name='eth_4h_diff', table=eth_table, messenger=eth_4hour_diff_chat,
+                                   analyzer=analyzer, test=should_test)
+    eth_diff_4h.set_threshold(0)
+
+    alerts = [eth_diff_1m, eth_diff_1h, eth_diff_4h]
+
+    error_chat = TelegramChat(chat_id=OTHER_CHAT_IDS['JayBotErrors'])
+
+    first_run = True
 
     while True:
         seconds_into_minute = time.localtime().tm_sec
+        if first_run:
+            logging.info(f'== Starting Alerts in {60 - seconds_into_minute} seconds .... ==')
+            first_run = False
         time.sleep(60 - seconds_into_minute)
-        logging.debug('== START ==')
 
         if util.get_host() == 'jaybizserver':
             time.sleep(3)
